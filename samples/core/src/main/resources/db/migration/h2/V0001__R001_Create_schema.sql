15359d53514714b59d5b8103597f52e67c4aff86
Jakub Zmuda <jakub.zmuda@capgemini.com>
2018-11-28 15:58:13 +0100

    commit3

--- samples/core/src/main/resources/db/migration/h2/V0001__R001_Create_schema.sql
+++ samples/core/src/main/resources/db/migration/h2/V0001__R001_Create_schema.sql
@@ -151,3 +151,13 @@ CREATE TABLE RevInfo(
   timestamp BIGINT NOT NULL,
   userLogin VARCHAR(255)
 );
+
+CREATE TABLE Supplier(
+    id BIGINT NOT NULL,
+    modificationCounter INTEGER NOT NULL,
+    name VARCHAR(255),
+    description VARCHAR(255),
+    rate INTEGER,
+    CONSTRAINT PK_Supplier PRIMARY KEY(id),
+    CONSTRAINT UC_Supplier_name UNIQUE(name)
+);
